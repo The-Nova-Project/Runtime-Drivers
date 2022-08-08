59ca59c35755ce0b4f2c53b35e890a630ed77b6b
shahzaib2028 <mohammadshahzaib2028@gmail.com>
2022-08-08 22:03:26 +0500

    edit

--- uart_with_socket.c
+++ uart_with_socket.c
@@ -146,51 +146,93 @@ int main(int argc, char **argv){
 
 
 
+  
 
 
 
-  int sock = 0, client_fd;
-	struct sockaddr_in serv_addr;
+
+
+
+
+
+  int server_fd, new_socket ;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	// char buffer[1024] = { 0 };
+	// char* hello = "Hello from server";
+
   char str[100];
   sprintf(str, "%d", write_value);
 	char* hello = str;
-	// char hello[100];
-	// printf("Enter Your Msg\n");
-	// scanf("%[^\n]%*c", hello);
-	// char buffer[1024] = { 0 };
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		return -1;
+
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))
+		== 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
 	}
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-
-	// Convert IPv4 and IPv6 addresses from text to binary
-	// form
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-		<= 0) {
-		printf(
-			"\nInvalid address/ Address not supported \n");
-		return -1;
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET,
+				SO_REUSEADDR , &opt,
+				sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
 	}
 
-	if ((client_fd
-		= connect(sock, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
+
+	if (setsockopt(server_fd, SOL_SOCKET,
+				 SO_REUSEPORT, &opt,
+				sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address))
 		< 0) {
-		printf("\nConnection Failed \n");
-		return -1;
+		perror("bind failed");
+		exit(EXIT_FAILURE);
 	}
-	send(sock, hello, strlen(hello), 0);
-	printf("Your message sent\n");
-	// valread = read(sock, buffer, 1024);
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	if ((new_socket
+		= accept(server_fd, (struct sockaddr*)&address,
+				(socklen_t*)&addrlen))
+		< 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	// valread = read(new_socket, buffer, 1024);
 	// printf("%s\n", buffer);
 
-	// closing the connected socket
-	close(client_fd);
+	// char hello[100];
+	// scanf("%[^\n]%*c", hello);
+	
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Your message sent\n");
+	
+// closing the connected socket
+	close(new_socket);
+// closing the listening socket
+	shutdown(server_fd, SHUT_RDWR);
 	return 0;
 
+
+
+
+  
 
 
 
