ede88ebd25f642fbd6a3b01958ec73dbe1e033b2
shahzaib2028 <mohammadshahzaib2028@gmail.com>
2022-08-10 22:47:11 +0500

    minor changes

--- uart_to_uart.c
+++ uart_to_uart.c
@@ -11,31 +11,19 @@
 #include <utils/sh_dpi_tasks.h>
 #include <time.h>
 
-// VDip Switches for Hydra SU Reset
-// #define SU_RESET_OFF            0x02
-// #define SU_RESET_ON             0x03
-// #define DEADBEEF                0xdeadbeef
-#define WAIT_DELAY1             1200
-#define WAIT_DELAY2             100
-#define tx_enable_interrupt     0x0004000C      //for bar1 transmit enable interrupt
-#define tx_data                 0x00000         //for bar1 tx data
-#define tx_data_poke            0x00004         //for bar1 tx data
-#define tx_sts                  0x00008      //UART1 status reg
-#define rx_enable_interrupt     0x0000C      ////for bar1 receive enable interrupt
-#define rx_data                 0x000000        //read data from receiver
-#define rx_valid_status         0x0             //by default value is 0
-#define rx_sts                  0x000008        //UART2 status reg
-#define tx_empty_status         0x0
+#define tx_uart                     0x00000
+#define tx_transmit_reg             0x00004
+#define rx_uart                     0x80000
+#define rx_uart_control_reg         0x8000C
+#define rx_uart_status_reg          0x80008
+
+
 
 #define interrupt_value         0x10
-#define data_value              0xabc     
+#define data_value              0xa
+#define WAIT_DELAY2             100
 
-#define ZERO_VAL                0x00
-#define TO_HOST                 0x120
-#define BRAM_START_ADDR         0x0
-#define TOTAL_INSTR             75
-#define PASSED_VAL              0
-#define FAILED_VAL              1
+
 
 const struct logger *logger             = &logger_stdout;
 static uint16_t pci_vendor_id           = 0x1D0F; /* Amazon PCI Vendor ID */
@@ -95,26 +83,21 @@ int check_afi_ready(int slot_id) {
 int main(int argc, char **argv){
 
 
-    // uint8_t   reset_su          = 0U;
-    // uint16_t  dip_sw_val        = 0U;
-    // uint32_t tx_control_reg     = tx_enable_interrupt;
-    uint32_t intrrupt_enable_value        = interrupt_value
-    uint32_t rx_control_reg     = rx_enable_interrupt;
-    // uint32_t   tx_status_reg       = tx_sts;
-    // uint16_t   dip_sw_val          = 0U;
+  
+    
     uint32_t   tx_data_reg         = tx_data;
-    uint32_t   tx_reg_poke         = tx_data_poke;
-    // uint32_t   rx_data_reg         = rx_data;
-    uint32_t   rx_status_reg       = tx_sts;
-    // uint32_t   data_write_value    = data_value;
-    uint32_t   write_value         = tx_empty_status;
+    uint32_t   send_data           = data_value;
+    uint32_t   tx_transmit         = tx_transmit_reg;
+    uint32_t   intrrupt_enable_value = interrupt_value;
+    uint32_t   rx_control          = rx_uart_control_reg
+    uint32_t   rx_status           = rx_uart_status_reg
+    uint32_t   rx_data_read        = rx_uart
+    uint32_t   write_value         ;
     int        for_AND             = 16;
     int        result_AND         ;
     uint32_t   intr_sts            ;
-    // uint32_t   valid_status        = rx_valid_status;
-    // uint16_t   led_val             = 0U;               
     int        slot_id             = 0, 
-//                bar_id              = APP_PF_BAR1,
+               bar_id              = APP_PF_BAR1,
                bar_id2             = APP_PF_BAR0,
                rc;
             //   opt;
@@ -128,33 +111,45 @@ int main(int argc, char **argv){
     rc = check_afi_ready(slot_id);
     fail_on(rc, out, "AFI not ready");
 
-    rc = fpga_pci_attach(slot_id, pf_id, bar_id2, 0, &pci_bar_handle);
+    rc = fpga_pci_attach(slot_id, pf_id, bar_id, 0, &pci_bar_handle);
     fail_on(rc, out, "Unable to attach to the AFI on slot id %d", slot_id);
 
 
-
-
-
-    printf("\n ------ ---- --- --- -- - -- reading data from RX  ---- --- -- -- - -- - - - --- - \n");
+    printf("\n ------ ---- --- --- -- - -- sending data ---- --- -- -- - -- - - - --- - \n");
 
     
 
-    printf("writing 0x%08x to RX register \n", intrrupt_enable_value);    //interrupt enable rx
-    rc = fpga_pci_poke(pci_bar_handle, rx_control_reg, intrrupt_enable_value);
+    printf("writing 0x%08x to transmit register \n", send_data);    //interrupt enable rx
+    rc = fpga_pci_poke(pci_bar_handle, tx_transmit, send_data);
     fail_on(rc, out, "Unable to write to the fpga !");
     printf("\n");
 
     printf("SLEEP FOR %4ld microecond \n", delayValue2);                              //time sleep
     usleep(delayValue2);
 
-    printf("SLEEP FOR %4ld microecond \n", delayValue2);                              //time sleep
-    usleep(delayValue2);
+
+
+
+
+    printf("\n ------ ---- --- --- -- - -- receiving data ---- --- -- -- - -- - - - --- - \n");
+
+
+
+    rc = fpga_pci_attach(slot_id, pf_id, bar_id2, 0, &pci_bar_handle);
+    fail_on(rc, out, "Unable to attach to the AFI on slot id %d", slot_id);
+
+
+    printf("writing 0x%08x to enable interrupt \n", intrrupt_enable_value);    //interrupt enable rx
+    rc = fpga_pci_poke(pci_bar_handle, rx_control, intrrupt_enable_value);
+    fail_on(rc, out, "Unable to write to the fpga !");
+    printf("\n")
+
 
    
-   rc = fpga_pci_peek(pci_bar_handle, rx_status_reg, &intr_sts);
+   rc = fpga_pci_peek(pci_bar_handle, rx_status, &intr_sts);
     fail_on(rc, out, "Unable to read read from the fpga !");
     printf(" valid value is  - 0x%08x", intr_sts);
-    printf("\n");
+    printf("\n")
 
 
     result_AND = intr_sts & for_AND;
@@ -162,24 +157,24 @@ int main(int argc, char **argv){
 
     while (result_AND != 16)
     {
-        rc = fpga_pci_peek(pci_bar_handle, rx_status_reg, &intr_sts);
+        rc = fpga_pci_peek(pci_bar_handle, rx_status, &intr_sts);
         fail_on(rc, out, "Unable to read read from the fpga !");
         
         result_AND = intr_sts & for_AND;
         
     }
 
-    printf("Exit from loop ");
-    printf("\n");
+    printf("Exit from loop ")
+    printf("\n")
 
     printf("valid value is  - 0x%08x", intr_sts);
-    printf("\n");
+    printf("\n")
     
 
     
 
 
-    rc = fpga_pci_peek(pci_bar_handle, tx_data_reg, &write_value);
+    rc = fpga_pci_peek(pci_bar_handle, rx_data_read, &write_value);
     fail_on(rc, out, "Unable to read read from the fpga !");                                 //read byte received
 
     printf("value received is - '%c'", write_value);
