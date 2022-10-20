38c8bd8135c4faf3e11e4443c6be20f10fe0d99c
shahzaibk23 <shahzaibceo@gmail.com>
2022-10-20 19:53:04 +0500

    uart on top

--- uart_socket_thread.c
+++ uart_socket_thread.c
@@ -36,56 +36,6 @@ pthread_t transmitter_thread;
 int alwaystrue = 1;
 
 
-
-
-int main(){
-
-    // pthread_t   transmitter_thread;
-    int         server_fd, 
-                new_socket, 
-                valread;
-    struct      sockaddr_in address;
-    int         opt = 1,
-                alwaystrue = 1,
-                addrlen = sizeof(address);
-    char        buffer[1024] = {0};
-    char*       rcvd_val = "";
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-
-        pthread_create(&transmitter_thread, NULL, uart_transmitter, NULL);
-        pthread_join(transmitter_thread, NULL);
-
-
-    return 0;
-
-}
-
-
-
 void* uart_transmitter(){
 
     uint32_t   send_data;
@@ -147,6 +97,56 @@ out:
 
     
 }
+
+
+int main(){
+
+    // pthread_t   transmitter_thread;
+    int         server_fd, 
+                new_socket, 
+                valread;
+    struct      sockaddr_in address;
+    int         opt = 1,
+                alwaystrue = 1,
+                addrlen = sizeof(address);
+    char        buffer[1024] = {0};
+    char*       rcvd_val = "";
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+
+        pthread_create(&transmitter_thread, NULL, uart_transmitter, NULL);
+        pthread_join(transmitter_thread, NULL);
+
+
+    return 0;
+
+}
+
+
+
 
 
 uint32_t uart_receiver(){
